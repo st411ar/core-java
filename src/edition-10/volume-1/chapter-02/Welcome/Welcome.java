0d643deff39d8f1d318a9c8793486f1df53a85f0
Vitaly Kiselev <st411ar@ya.ru>
2022-02-13 19:14:40 +0300

    yo snake fix

--- src/edition-10/volume-1/chapter-02/Welcome/Welcome.java
+++ src/edition-10/volume-1/chapter-02/Welcome/Welcome.java
@@ -11,7 +11,7 @@ public class Welcome
       System.out.println(greeting);
       for (int i = 0; i < greeting.length(); i++)
          System.out.print("=");
-      System.out.println("Yo, Snake!");
       System.out.println();
+      System.out.println("Yo, Snake!");
    }
 }
